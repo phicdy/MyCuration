4f324c1ec592a62c37d57c3d91773438949f0b31
phicdy <phicdy@gmail.com>
2014-06-25 18:17:13 +0900

    Execute all task in each thread

--- src/com/pluea/rssfilterreader/task/UpdateFeedsTask.java
+++ src/com/pluea/rssfilterreader/task/UpdateFeedsTask.java
@@ -1,5 +1,6 @@
 package com.pluea.rssfilterreader.task;
-  
+
+import java.io.IOException;
 import java.util.ArrayList;
 
 import com.example.rssfilterreader.R;
@@ -17,148 +18,172 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.Toast;
-  
+
 /**
  * 
  * @author kyamaguchi
- * @param  String : data type for task(ex.download URL)
- * @param  String : data type for displaying the progress
- * @param  Integer : submit data type when task will finish 
+ * @param String
+ *            : data type for task(ex.download URL)
+ * @param String
+ *            : data type for displaying the progress
+ * @param Integer
+ *            : submit data type when task will finish
  */
-public class UpdateFeedsTask extends AsyncTask<ArrayList<Feed>, String, Boolean>{
-  
-    private Context context_;
-    private ProgressDialog progress_;
-    private RssParser rssParser;
-    private FilterTask filterTask;
-    private DatabaseAdapter dbAdapter;
-    private Intent intent = new Intent();
-    private ArrayList<Feed> feeds;
-    private int numOfUnreadArticles;
-    private int feedId;
-    private boolean showProgress = false;
-    private boolean isRunning = false;
-    
-    //Singleton
-    private static UpdateFeedsTask updateTask;
-    
-    private static final String LOG_TAG = "RSS_READER."+UpdateFeedsTask.class.getName();
-    
-    private UpdateFeedsTask(Context context, boolean showProgress) {
-        Log.i(LOG_TAG, "Create instance");
-        context_  = context;
-        rssParser  = new RssParser(context_);
-        filterTask = new FilterTask(context_);
-        dbAdapter  = new DatabaseAdapter(context_);
-        this.showProgress = showProgress;
-        isRunning = false;
-    }
-  
-    public static UpdateFeedsTask getInstance(Context context, boolean showProgress) {
-    	if(updateTask == null) {
-    		updateTask = new UpdateFeedsTask(context, showProgress);
-    	}
-    	return updateTask;
-    }
-    
-    /**
-     * Execute on main thread
-     */
-    @Override
-    protected void onPostExecute(Boolean result) {
-    	if(result) {
-    		if(!feeds.isEmpty() && showProgress) {
-    			Toast.makeText(context_, R.string.article_updated, Toast.LENGTH_SHORT).show();
-    		}
-    	}else if(showProgress){
-    		Toast.makeText(context_, R.string.article_update_error, Toast.LENGTH_SHORT).show();
-    	}
-    	//Broadcast updating num of articles
+public class UpdateFeedsTask extends
+		AsyncTask<ArrayList<Feed>, String, Boolean> {
+
+	private Context context_;
+	private ProgressDialog progress_;
+	private RssParser rssParser;
+	private FilterTask filterTask;
+	private DatabaseAdapter dbAdapter;
+	private Intent intent = new Intent();
+	private ArrayList<Feed> feeds;
+	private int numOfUnreadArticles;
+	private int feedId;
+	private boolean showProgress = false;
+	private boolean isRunning = false;
+
+	// Singleton
+	private static UpdateFeedsTask updateTask;
+
+	private static final String LOG_TAG = "RSS_READER."
+			+ UpdateFeedsTask.class.getName();
+
+	private UpdateFeedsTask(Context context, boolean showProgress) {
+		Log.i(LOG_TAG, "Create instance");
+		context_ = context;
+		rssParser = new RssParser(context_);
+		filterTask = new FilterTask(context_);
+		dbAdapter = new DatabaseAdapter(context_);
+		this.showProgress = showProgress;
+		isRunning = false;
+	}
+
+	public static UpdateFeedsTask getInstance(Context context,
+			boolean showProgress) {
+		if (updateTask == null) {
+			updateTask = new UpdateFeedsTask(context, showProgress);
+		}
+		return updateTask;
+	}
+
+	/**
+	 * Execute on main thread
+	 */
+	@Override
+	protected void onPostExecute(Boolean result) {
+		if (result) {
+			if (!feeds.isEmpty() && showProgress) {
+				Toast.makeText(context_, R.string.article_updated,
+						Toast.LENGTH_SHORT).show();
+			}
+		} else if (showProgress) {
+			Toast.makeText(context_, R.string.article_update_error,
+					Toast.LENGTH_SHORT).show();
+		}
+		// Broadcast updating num of articles
 		intent.setAction(FeedListActivity.UPDATE_NUM_OF_ARTICLES);
 		context_.sendBroadcast(intent);
-    	
-		if(showProgress) {
+
+		if (showProgress) {
 			progress_.dismiss();
 		}
-    	
-    	//initialize task because task can execute only one time
-    	updateTask = null;
-    }
-    /**
-     * Execute before doing task
-     */
-    @Override
-    protected void onPreExecute() {
-        //Display a dialog
-    	if(showProgress) {
-	        progress_ = new ProgressDialog(context_);
-	        progress_.setMessage("Now loading");
-	        progress_.show();
-    	}
-    }
-  
-    @Override
-    protected void onProgressUpdate(String... values) {
-        // TODO Auto-generated method stub
-        super.onProgressUpdate(values);
-    }
-      
-    /**
-     * Get articles from RSS Feed
-     */
-    @SuppressLint("DefaultLocale")
+
+		isRunning = false;
+		// initialize task because task can execute only one time
+		updateTask = null;
+	}
+
+	/**
+	 * Execute before doing task
+	 */
 	@Override
-    protected Boolean doInBackground(ArrayList<Feed>... feedsValue ) {
-        try {
-        	feeds = feedsValue[0];
-        	for(Feed feed : feeds) {
-	            //Set URL string and id
-	            String urlString = feed.getUrl();
-	            feedId = feed.getId();
-	            
-	            ArrayList<Article> articles = dbAdapter.getUnreadArticlesInAFeed(feedId);
-	            for(int i=0;i<articles.size();i++) {
-	    			Article article = articles.get(i);
-	    			article.setArrayIndex(i);
-	    			GetHatenaBookmarkPointTask hatenaTask = new GetHatenaBookmarkPointTask(context_);
-	    			hatenaTask.execute(article);
-	    		}
-	            
-	        	//Parse XML
-	            boolean parseResult = rssParser.parseXml(urlString, feedId);
-	            
-	            //Update articles "toRead" status to "read"
-	            if(!dbAdapter.changeArticlesStatusToRead()) {
-	            	return false;
-	            }
-	            
-	            //Filter articles
-	            if(parseResult) {
-	                if(filterTask.applyFiltering(feedId)) {
-	            		continue;
-	                }else {
-	                	return false;
-	                }
-	            }else {
-	                return false;
-	            }
-        	}
-        	return true;
-        }catch(Exception e) {
-    		e.printStackTrace();
-        }
-        return false;
-    }
+	protected void onPreExecute() {
+		// Display a dialog
+		if (showProgress) {
+			progress_ = new ProgressDialog(context_);
+			progress_.setMessage("Now loading");
+			progress_.show();
+		}
+	}
+
+	@Override
+	protected void onProgressUpdate(String... values) {
+		// TODO Auto-generated method stub
+		super.onProgressUpdate(values);
+	}
+
+	/**
+	 * Get articles from RSS Feed
+	 */
+	@SuppressLint("DefaultLocale")
+	@Override
+	protected Boolean doInBackground(ArrayList<Feed>... feedsValue) {
+		isRunning = true;
+		try {
+			feeds = feedsValue[0];
+			for (final Feed feed : feeds) {
+				Thread thread = new Thread(new Runnable() {
+
+					@Override
+					public void run() {
+						// Set URL string and id
+						String urlString = feed.getUrl();
+						feedId = feed.getId();
+
+						ArrayList<Article> articles = dbAdapter
+								.getUnreadArticlesInAFeed(feedId);
+						for (int i = 0; i < articles.size(); i++) {
+							Article article = articles.get(i);
+							article.setArrayIndex(i);
+							GetHatenaBookmarkPointTask hatenaTask = new GetHatenaBookmarkPointTask(
+									context_);
+							hatenaTask.execute(article);
+						}
+
+						// Parse XML
+						boolean parseResult;
+						try {
+							parseResult = rssParser.parseXml(urlString,
+									feedId);
+							// Update articles "toRead" status to "read"
+							if (!dbAdapter.changeArticlesStatusToRead()) {
+								return;
+							}
+							
+							// Filter articles
+							if (parseResult) {
+								filterTask.applyFiltering(feedId);
+							} else {
+								return;
+							}
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+
+						// Broadcast updating num of articles
+						intent.setAction(FeedListActivity.UPDATE_NUM_OF_ARTICLES);
+						context_.sendBroadcast(intent);
+					}
+				});
+				thread.start();
+			}
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
 
 	@Override
 	protected void onCancelled() {
 		Log.i(LOG_TAG, "task is canceled complete");
 		super.onCancelled();
 	}
-	
+
 	public boolean isRunning() {
 		return isRunning;
 	}
-     
 
 }
