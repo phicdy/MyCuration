1a7cc023e85e9195da1f69ee4abfd82db3cd6970
phicdy <phicdy@gmail.com>
2014-07-06 18:38:02 +0900

    Fix auto update feed alarm

--- src/com/pluea/rssfilterreader/alarm/AlarmManagerTaskManager.java
+++ src/com/pluea/rssfilterreader/alarm/AlarmManagerTaskManager.java
@@ -7,6 +7,7 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class AlarmManagerTaskManager {
 
@@ -18,12 +19,15 @@ public class AlarmManagerTaskManager {
 	
 	public static void setNewAlarm(Context context) {
 		Intent i = new Intent(context, AutoUpdateBroadcastReciever.class);
+		i.setAction(AutoUpdateBroadcastReciever.AUTO_UPDATE_ACTION);
+		
 		PendingIntent pi = PendingIntent.getBroadcast(context, 0, i, 0);
 		AlarmManager alm = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
 		
 		Calendar calendar = Calendar.getInstance();
 		calendar.setTimeInMillis(System.currentTimeMillis());
 		calendar.add(Calendar.SECOND, INTERVAL);
+		Log.d("AlarmManagerTaskManager", "Set alarm : " + calendar.getTime().toString());
 		
 		alm.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pi);
 	}
--- src/com/pluea/rssfilterreader/alarm/AutoUpdateBroadcastReciever.java
+++ src/com/pluea/rssfilterreader/alarm/AutoUpdateBroadcastReciever.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 import com.pleua.rssfilterreader.rss.Feed;
 import com.pluea.rssfilterreader.db.DatabaseAdapter;
@@ -12,16 +13,21 @@ import com.pluea.rssfilterreader.task.UpdateTaskManager;
 
 public class AutoUpdateBroadcastReciever extends BroadcastReceiver {
 
+	public static final String AUTO_UPDATE_ACTION = "autoUpdateFeed";
+	
 	DatabaseAdapter dbAdapter;
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		dbAdapter = new DatabaseAdapter(context);
-		UpdateTaskManager updateTask = UpdateTaskManager.getInstance(context);
-
-		updateTask.updateAllFeeds(dbAdapter.getAllFeeds());
-		
-		// Save new time
-		AlarmManagerTaskManager.setNewAlarm(context);
+		Log.d("AutoUpdateBroadcastReciever", "onReceive");
+		if(intent.getAction().equals(AUTO_UPDATE_ACTION)) {
+			dbAdapter = new DatabaseAdapter(context);
+			UpdateTaskManager updateTask = UpdateTaskManager.getInstance(context);
+	
+			updateTask.updateAllFeeds(dbAdapter.getAllFeeds());
+			
+			// Save new time
+			AlarmManagerTaskManager.setNewAlarm(context);
+		}
 	}
 }
