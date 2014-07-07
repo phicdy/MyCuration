32efb13395b31d6e64bf3ad0ff129b7d0a022523
phicdy <phicdy@gmail.com>
2014-07-07 22:52:22 +0900

    Add SettingActivity

---
+++ src/com/pluea/rssfilterreader/ui/SettingActivity.java
@@ -0,0 +1,29 @@
+package com.pluea.rssfilterreader.ui;
+  
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.rssfilterreader.R;
+  
+public class SettingActivity extends Activity {
+  
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+	}
+    
+    
+}
