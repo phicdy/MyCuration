dad7db249a89fc964b10baf11d907b614d1e6307
phicdy <phicdy@gmail.com>
2014-07-10 23:41:30 +0900

    Add save button and set saved value as default

--- src/com/pluea/rssfilterreader/ui/SettingActivity.java
+++ src/com/pluea/rssfilterreader/ui/SettingActivity.java
@@ -2,8 +2,13 @@ package com.pluea.rssfilterreader.ui;
   
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.rssfilterreader.R;
+import com.pluea.rssfilterreader.util.PreferenceManager;
   
 public class SettingActivity extends Activity {
   
@@ -11,7 +16,30 @@ public class SettingActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        
+        initView();
+        setLitener();
 	}
+    
+    private void initView() {
+    	EditText input = (EditText)findViewById(R.id.inputInterval);
+    	int savedInterval = PreferenceManager.getInstance(getApplicationContext()).getAutoUpdateInterval();
+    	input.setText(String.valueOf(savedInterval));
+    }
+    
+    private void setLitener() {
+    	Button saveButton = (Button)findViewById(R.id.saveButton);
+    	saveButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				EditText intervalText = (EditText)findViewById(R.id.inputInterval);
+				String input = intervalText.getText().toString();
+				PreferenceManager mgr = PreferenceManager.getInstance(getApplicationContext());
+				mgr.setAutoUpdateInterval(Integer.valueOf(input));
+			}
+		});
+    }
 
 	@Override
 	protected void onResume() {
