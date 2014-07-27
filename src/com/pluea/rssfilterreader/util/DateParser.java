9db359b81895609a02bf16c0be53f2601c362089
phicdy <phicdy@gmail.com>
2014-07-27 16:49:06 +0900

    Add W3CDTF date format parse

--- src/com/pluea/rssfilterreader/util/DateParser.java
+++ src/com/pluea/rssfilterreader/util/DateParser.java
@@ -13,21 +13,49 @@ public class DateParser {
 
 	private static final String LOG_TAG = "DateParser";
 
-	private static Date getPubDate(String pubDate) {
+	private static Date parseDate(String pubDate) {
 		DateFormat input = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss Z",
 				Locale.US);
+		Date formatWithPubDate = null;
 		try {
-			return input.parse(pubDate);
+			formatWithPubDate = input.parse(pubDate);
 		} catch (ParseException e) {
 			e.printStackTrace();
+		}
+		if(formatWithPubDate == null) {
 			//2014-06-25 17:24:07
+			// TODO: set device locale
 			DateFormat noTimezone = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss",
 					Locale.JAPAN);
+			Date formatWithNoTimeZone = null;
 			try {
-				return noTimezone.parse(pubDate);
+				formatWithNoTimeZone = noTimezone.parse(pubDate);
 			} catch (ParseException e2) {
 				e2.printStackTrace();
 			}
+			if(formatWithNoTimeZone == null) {
+				//2014-07-27T14:38:34+09:00
+				if(pubDate.contains("T")) {
+					String replaced = pubDate.replace("T", " ");
+					DateFormat w3cdtf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ssZ",
+							Locale.US);
+					Date formatWithW3cdtf = null;
+					try {
+						formatWithW3cdtf = w3cdtf.parse(replaced);
+					} catch (ParseException e) {
+						e.printStackTrace();
+					}
+					if(formatWithW3cdtf == null) {
+						return null;
+					}else {
+						return formatWithW3cdtf;
+					}
+				}
+			}else {
+				return formatWithNoTimeZone;
+			}
+		}else {
+			return formatWithPubDate;
 		}
 		return null;
 	}
@@ -35,7 +63,10 @@ public class DateParser {
 	public static long changeToJapaneseDate(String dateBeforeChange) {
 		Log.d(LOG_TAG, "date before change:" + dateBeforeChange);
 		Calendar cal = Calendar.getInstance();
-		Date date = getPubDate(dateBeforeChange);
+		Date date = parseDate(dateBeforeChange);
+		if(date == null) {
+			return 0;
+		}
 		Log.d(LOG_TAG, date.toString());
 		cal.setTime(date);
 
