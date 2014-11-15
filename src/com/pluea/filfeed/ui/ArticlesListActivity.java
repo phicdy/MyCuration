c231c9b0ed555f8bcb9c036509e8b7a1c6065785
phicdy <phicdy@gmail.com>
2014-11-16 01:11:40 +0900

    Update article status to read in worker thread when all articles become to be read by next article button

--- src/com/pluea/filfeed/ui/ArticlesListActivity.java
+++ src/com/pluea/filfeed/ui/ArticlesListActivity.java
@@ -282,11 +282,18 @@ public class ArticlesListActivity extends ListActivity {
 						}
 					}
 					if(isAllRead) {
-						if(feedId == Feed.ALL_FEED_ID) {
-							dbAdapter.saveAllStatusToRead();
-						}else {
-							dbAdapter.saveStatusToRead(feedId);
-						}
+						new Thread(new Runnable() {
+							
+							@Override
+							public void run() {
+								if(feedId == Feed.ALL_FEED_ID) {
+									dbAdapter.saveAllStatusToRead();
+								}else {
+									dbAdapter.saveStatusToRead(feedId);
+								}
+								
+							}
+						}).start();
 						finish();
 					}
 				}
