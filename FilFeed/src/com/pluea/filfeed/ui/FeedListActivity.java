8eb376b822e75b8e72dd47acb1e1998ec730cf1a
phicdy <phicdy@gmail.com>
2014-12-30 02:11:40 +0900

    Change file name

--- FilFeed/src/com/pluea/filfeed/ui/FeedListActivity.java
+++ FilFeed/src/com/pluea/filfeed/ui/FeedListActivity.java
@@ -67,7 +67,7 @@ public class FeedListActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main_activity);
+		setContentView(R.layout.activity_feed_list);
 
 		feedsListView = (PullToRefreshListView) findViewById(R.id.feedList);
 		
