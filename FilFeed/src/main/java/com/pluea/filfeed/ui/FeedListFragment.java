bca412a7d7f3451be0cc8445f31ccff1520a7233
phicdy <phicdy@gmail.com>
2015-02-08 21:56:33 +0900

    Remove unused comment

--- FilFeed/src/main/java/com/pluea/filfeed/ui/FeedListFragment.java
+++ FilFeed/src/main/java/com/pluea/filfeed/ui/FeedListFragment.java
@@ -216,16 +216,6 @@ public class FeedListFragment extends Fragment {
                 .setRefreshingLabel(getString(R.string.loading) + "(" + updatedFeed + "/" + numOfAllFeeds + ")");
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFeedListFragmentListener {
         // TODO: Update argument type and name
         public void onListClicked(int position);
