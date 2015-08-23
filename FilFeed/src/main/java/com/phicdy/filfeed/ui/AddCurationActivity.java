c7f5e68c2f688a1a12fe368d460ac93535014a89
phicdy <phicdy@gmail.com>
2015-08-23 22:09:02 +0900

    Add menu

--- FilFeed/src/main/java/com/phicdy/filfeed/ui/AddCurationActivity.java
+++ FilFeed/src/main/java/com/phicdy/filfeed/ui/AddCurationActivity.java
@@ -24,14 +24,12 @@ public class AddCurationActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case R.id.add_curation:
+                break;
+            default:
+                break;
         }
 
         return super.onOptionsItemSelected(item);
