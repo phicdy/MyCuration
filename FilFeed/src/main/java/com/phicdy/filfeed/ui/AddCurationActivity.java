61da3e53b88c0759bf45643d8bf7be7f91a172f8
phicdy <phicdy@gmail.com>
2015-08-25 19:35:34 +0900

    Set listener for each view

--- FilFeed/src/main/java/com/phicdy/filfeed/ui/AddCurationActivity.java
+++ FilFeed/src/main/java/com/phicdy/filfeed/ui/AddCurationActivity.java
@@ -4,15 +4,34 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.phicdy.filfeed.R;
+import com.phicdy.filfeed.db.DatabaseAdapter;
+import com.phicdy.filfeed.util.TextUtil;
+import com.phicdy.filfeed.util.ToastHelper;
+
+import java.util.ArrayList;
 
 public class AddCurationActivity extends ActionBarActivity {
+
+    private CurationWordListFragment wordListFragment;
+    private Button btnAdd;
+    private EditText etInput;
+    private EditText etName;
+
+    private DatabaseAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_curation);
+        initView();
+        setAllListener();
+        adapter = DatabaseAdapter.getInstance(getApplicationContext());
     }
 
     @Override
@@ -27,11 +46,58 @@ public class AddCurationActivity extends ActionBarActivity {
         int id = item.getItemId();
         switch (id) {
             case R.id.add_curation:
+                if (insertCurationIntoDb()) {
+                    ToastHelper.showToast(getApplicationContext(), getString(R.string.curation_added_success), Toast.LENGTH_SHORT);
+                    finish();
+                }else {
+                    ToastHelper.showToast(getApplicationContext(), getString(R.string.curation_added_error), Toast.LENGTH_SHORT);
+                }
                 break;
             default:
                 break;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void initView() {
+        wordListFragment = (CurationWordListFragment)getSupportFragmentManager().findFragmentById(R.id.fr_curation_condition);
+        btnAdd = (Button)findViewById(R.id.btn_add_word);
+        etInput = (EditText)findViewById(R.id.et_curation_word);
+        etName = (EditText)findViewById(R.id.et_curation_name);
+    }
+
+    private void setAllListener() {
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String word = etInput.getText().toString();
+                if (word == null || word.equals("")) {
+                    ToastHelper.showToast(getApplicationContext(), getString(R.string.empty_word), Toast.LENGTH_SHORT);
+                    return;
+                }
+                wordListFragment.add(word);
+                etInput.setText("");
+            }
+        });
+
+    }
+
+    private boolean insertCurationIntoDb() {
+        String curationName = etName.getText().toString();
+        if (TextUtil.isEmpty(curationName)) {
+            ToastHelper.showToast(getApplicationContext(), getString(R.string.empty_curation_name), Toast.LENGTH_SHORT);
+            return false;
+        }
+        ArrayList<String> wordList = wordListFragment.getWordList();
+        if (wordList == null || wordList.size() == 0) {
+            ToastHelper.showToast(getApplicationContext(), getString(R.string.empty_word_list), Toast.LENGTH_SHORT);
+            return false;
+        }
+        if (adapter.isExistSameNameCuration(curationName)) {
+            ToastHelper.showToast(getApplicationContext(), getString(R.string.duplicate_curation_name), Toast.LENGTH_SHORT);
+            return false;
+        }
+        return adapter.saveNewCuration(curationName, wordList);
     }
 }
--- FilFeed/src/main/java/com/phicdy/filfeed/ui/CurationWordListFragment.java
+++ FilFeed/src/main/java/com/phicdy/filfeed/ui/CurationWordListFragment.java
@@ -2,42 +2,27 @@ package com.phicdy.filfeed.ui;
 
 import android.app.Activity;
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.handmark.pulltorefresh.library.PullToRefreshBase;
-import com.handmark.pulltorefresh.library.PullToRefreshListView;
 import com.phicdy.filfeed.R;
-import com.phicdy.filfeed.db.DatabaseAdapter;
-import com.phicdy.filfeed.rss.Feed;
-import com.phicdy.filfeed.rss.UnreadCountManager;
-import com.phicdy.filfeed.task.NetworkTaskManager;
-
-import java.io.File;
 import java.util.ArrayList;
 
 public class CurationWordListFragment extends Fragment {
 
-    private PullToRefreshListView feedsListView;
+    private ListView curationWordListView;
     private CurationWordListAdapter curationWordListAdapter;
-    private OnFeedListFragmentListener mListener;
 
-    private ArrayList<Feed> feeds = new ArrayList<>();
-    private ArrayList<Feed> allFeeds = new ArrayList<>();
-    private DatabaseAdapter dbAdapter;
+    private ArrayList<String> addedWords = new ArrayList<>();
 
     private static final String LOG_TAG = "FilFeed.CurationWordList";
 
@@ -51,13 +36,6 @@ public class CurationWordListFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        dbAdapter = DatabaseAdapter.getInstance(getActivity());
-        allFeeds = dbAdapter.getAllFeedsWithNumOfUnreadArticles();
-        // For show/hide
-        if (allFeeds.size() != 0) {
-            addShowHideLine(allFeeds);
-        }
     }
 
     @Override
@@ -70,180 +48,67 @@ public class CurationWordListFragment extends Fragment {
     @Override
     public void onPause() {
         super.onPause();
-        if (NetworkTaskManager.getInstance(getActivity()).isUpdatingFeed()) {
-            feedsListView.onRefreshComplete();
-        }
-    }
-
-    private void setAllListener() {
-        // When an feed selected, display unread articles in the feed
-        feedsListView
-                .setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view,
-                                            int position, long id) {
-                        mListener.onListClicked(position-1);
-                    }
-
-                });
-        feedsListView.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener<ListView>() {
-
-            @Override
-            public void onRefresh(PullToRefreshBase<ListView> refreshView) {
-                mListener.onRefreshList();
-            }
-        });
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_feed_list, container, false);
+        return inflater.inflate(R.layout.fragment_curation_word_list, container, false);
     }
 
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
-        try {
-            mListener = (OnFeedListFragmentListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        feedsListView = (PullToRefreshListView) getActivity().findViewById(R.id.feedList);
-        TextView emptyView = (TextView)getActivity().findViewById(R.id.emptyView);
-        feedsListView.setEmptyView(emptyView);
-        getActivity().registerForContextMenu(feedsListView.getRefreshableView());
-        setAllListener();
+        curationWordListView = (ListView) getActivity().findViewById(R.id.lv_curation_word);
 
         refreshList();
 
         // Set ListView
-        curationWordListAdapter = new CurationWordListAdapter(feeds, getActivity());
-        feedsListView.setAdapter(curationWordListAdapter);
+        curationWordListAdapter = new CurationWordListAdapter(addedWords, getActivity());
+        curationWordListView.setAdapter(curationWordListAdapter);
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
     }
 
     public void refreshList() {
-        curationWordListAdapter = new CurationWordListAdapter(allFeeds, getActivity());
-        feedsListView.setAdapter(curationWordListAdapter);
+        curationWordListAdapter = new CurationWordListAdapter(addedWords, getActivity());
+        curationWordListView.setAdapter(curationWordListAdapter);
         curationWordListAdapter.notifyDataSetChanged();
     }
 
-    public void addFeed(Feed newFeed) {
-        deleteShowHideLineIfNeeded();
-        if (newFeed.getUnreadAriticlesCount() > 0) {
-            feeds.add(newFeed);
-            addShowHideLine(feeds);
+    public void add(String word) {
+        if (addedWords.contains(word)) {
+            Toast.makeText(getActivity(), getString(R.string.duplicate_word), Toast.LENGTH_SHORT).show();
+            return;
         }
-        allFeeds.add(newFeed);
-        addShowHideLine(allFeeds);
+        addedWords.add(word);
         curationWordListAdapter.notifyDataSetChanged();
     }
 
-    private void deleteShowHideLineIfNeeded() {
-        if (feeds != null && feeds.size() > 0) {
-            int lastIndex = feeds.size() - 1;
-            if (feeds.get(lastIndex).getId() == Feed.DEFAULT_FEED_ID) {
-                feeds.remove(lastIndex);
-            }
-        }
-        if (allFeeds != null && allFeeds.size() > 0) {
-            int lastIndex = allFeeds.size() - 1;
-            if (allFeeds.get(lastIndex).getId() == Feed.DEFAULT_FEED_ID) {
-                allFeeds.remove(lastIndex);
-            }
-        }
+    public ArrayList<String> getWordList() {
+        return addedWords;
     }
 
-    private void addShowHideLine(ArrayList<Feed> feeds) {
-        feeds.add(new Feed());
-    }
-
-    public void removeFeedAtPosition(int position) {
-        Feed deletedFeed = allFeeds.get(position);
-        dbAdapter.deleteFeed(deletedFeed.getId());
-        allFeeds.remove(position);
-        for (int i = 0;i < feeds.size();i++) {
-            if (feeds.get(i).getId() == deletedFeed.getId()) {
-                feeds.remove(i);
-            }
-        }
+    public void removedWordAtPosition(int position) {
+        addedWords.remove(position);
         curationWordListAdapter.notifyDataSetChanged();
     }
 
-    public int getFeedIdAtPosition (int position) {
-        if (position < 0) {
-            return -1;
-        }
-
-        if (allFeeds == null || position > allFeeds.size()-1) {
-            return -1;
-        }
-        return allFeeds.get(position).getId();
-    }
-
-    public String getFeedTitleAtPosition (int position) {
-        if (position < 0 || feeds == null || position > feeds.size()-1) {
-            return null;
-        }
-        return allFeeds.get(position).getTitle();
-    }
-
-
-    public String getFeedUrlAtPosition (int position) {
-        if (position < 0) {
-            return null;
-        }
-        if (allFeeds == null || position > allFeeds.size()-1) {
-            return null;
-        }
-        return allFeeds.get(position).getUrl();
-    }
-
-    public void updateFeedTitle(int feedId, String newTitle) {
-        for (Feed feed : allFeeds) {
-            if (feed.getId() == feedId) {
-                feed.setTitle(newTitle);
-                break;
-            }
-        }
-        for (Feed feed : feeds) {
-            if (feed.getId() == feedId) {
-                feed.setTitle(newTitle);
-                break;
-            }
-        }
-        curationWordListAdapter.notifyDataSetChanged();
-    }
-
-    public interface OnFeedListFragmentListener {
-        public void onListClicked(int position);
-        public void onRefreshList();
-    }
-
-    /**
-     *
-     * @author kyamaguchi Display RSS Feeds List
-     */
-    class CurationWordListAdapter extends ArrayAdapter<Feed> {
-        public CurationWordListAdapter(ArrayList<Feed> feeds, Context context) {
-            super(context, R.layout.feeds_list, feeds);
+    class CurationWordListAdapter extends ArrayAdapter<String> {
+        public CurationWordListAdapter(ArrayList<String> words, Context context) {
+            super(context, R.layout.curation_word_list, words);
         }
 
         @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
+        public View getView(final int position, View convertView, ViewGroup parent) {
             ViewHolder holder;
 
             // Use contentView and setup ViewHolder
@@ -259,21 +124,14 @@ public class CurationWordListFragment extends Fragment {
                 holder = (ViewHolder) row.getTag();
             }
 
-            Feed feed = this.getItem(position);
-
-            String iconPath = feed.getIconPath();
-            holder.btnDelete.setVisibility(View.VISIBLE);
-            if (iconPath == null || iconPath.equals(Feed.DEDAULT_ICON_PATH)) {
-                holder.tvWord.setText(feed.getTitle());
-            }else {
-                File file = new File(iconPath);
-                if (file.exists()) {
-                    Bitmap bmp = BitmapFactory.decodeFile(file.getPath());
-                } else {
-                    dbAdapter.saveIconPath(feed.getSiteUrl(), Feed.DEDAULT_ICON_PATH);
+            String word = this.getItem(position);
+            holder.tvWord.setText(word);
+            holder.btnDelete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    removedWordAtPosition(position);
                 }
-                holder.tvWord.setText(feed.getTitle());
-            }
+            });
 
             return row;
         }
