4a2cc2e3a1138161b2f3f6afa07857e209d8e995
phicdy <phicdy@gmail.com>
2015-09-08 20:40:02 +0900

    Set initialization and listener to update the values

--- FilFeed/src/main/java/com/phicdy/filfeed/ui/SettingActivity.java
+++ FilFeed/src/main/java/com/phicdy/filfeed/ui/SettingActivity.java
@@ -1,91 +1,18 @@
 package com.phicdy.filfeed.ui;
-  
+
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.Spinner;
-import android.widget.Toast;
-
-import com.phicdy.filfeed.R;
-import com.phicdy.filfeed.alarm.AlarmManagerTaskManager;
-import com.phicdy.filfeed.util.PreferenceHelper;
 
 public class SettingActivity extends PreferenceActivity {
-  
-	private Spinner spUpdateInterval;
-	private CheckBox cbSortNewArticleTop;
-	private CheckBox cbAllReadBack;
-	private CheckBox cbOpenInternal;
-	private Spinner spSwipeDirection;
-	
+
+	private SettingFragment fragment = new SettingFragment();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
-//        initView();
-//        setLitener();
 		getFragmentManager().beginTransaction()
-				.replace(android.R.id.content, new SettingFragment())
+				.replace(android.R.id.content, fragment)
 				.commit();
-
 	}
-    
-    private void initView() {
-        setTitle(getString(R.string.setting));
-    	
-    	PreferenceHelper mgr = PreferenceHelper.getInstance(getApplicationContext());
-    	spUpdateInterval = (Spinner)findViewById(R.id.sp_update_interval);
-    	// Set stored position of value
-    	int autoUpdateIntervalSecond = mgr.getAutoUpdateIntervalSecond();
-    	int autoUpdateIntervalHour = autoUpdateIntervalSecond / (60 * 60);
-    	String updateIntervalHourItems[] = getResources().getStringArray(R.array.update_interval_items);
-    	for (int indexOfSpinner = 0; indexOfSpinner < updateIntervalHourItems.length; indexOfSpinner++) {
-    		if (Integer.valueOf(updateIntervalHourItems[indexOfSpinner]) == autoUpdateIntervalHour) {
-    			spUpdateInterval.setSelection(indexOfSpinner);
-    			break;
-    		}
-		}
-    	
-    	cbSortNewArticleTop = (CheckBox)findViewById(R.id.cb_article_sort);
-    	cbSortNewArticleTop.setChecked(mgr.getSortNewArticleTop());
-    	
-    	cbAllReadBack = (CheckBox)findViewById(R.id.cb_all_read_back);
-    	cbAllReadBack.setChecked(mgr.getAllReadBack());
-    	
-    	cbOpenInternal = (CheckBox)findViewById(R.id.cb_open_internal);
-    	cbOpenInternal.setChecked(mgr.isOpenInternal());
-    	
-    	spSwipeDirection = (Spinner)findViewById(R.id.sp_swipe_direction);
-    	spSwipeDirection.setSelection(mgr.getSwipeDirection());
-    }
-    
-    private void setLitener() {
-    	Button saveButton = (Button)findViewById(R.id.saveButton);
-    	saveButton.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				PreferenceHelper mgr = PreferenceHelper.getInstance(getApplicationContext());
-				
-				// Save update interval
-				int intervalHour = Integer.valueOf((String)spUpdateInterval.getSelectedItem());
-				int intervalSecond = intervalHour * 60 * 60;
-				mgr.setAutoUpdateIntervalSecond(intervalSecond);
-				
-				mgr.setSortNewArticleTop(cbSortNewArticleTop.isChecked());
-				mgr.setAllReadBack(cbAllReadBack.isChecked());
-				mgr.setOpenInternal(cbOpenInternal.isChecked());
-				
-				// Save swipe direction
-				mgr.setSwipeDirection(spSwipeDirection.getSelectedItemPosition());
-				
-				AlarmManagerTaskManager.setNewAlarm(getApplicationContext());
-				
-				Toast.makeText(getApplicationContext(), getString(R.string.saved), Toast.LENGTH_SHORT).show();
-			}
-		});
-    }
 
 }
--- FilFeed/src/main/java/com/phicdy/filfeed/ui/SettingFragment.java
+++ FilFeed/src/main/java/com/phicdy/filfeed/ui/SettingFragment.java
@@ -1,11 +1,25 @@
 package com.phicdy.filfeed.ui;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.PreferenceFragment;
+import android.preference.SwitchPreference;
 
 import com.phicdy.filfeed.R;
+import com.phicdy.filfeed.alarm.AlarmManagerTaskManager;
+import com.phicdy.filfeed.util.PreferenceHelper;
 
 public class SettingFragment extends PreferenceFragment {
+
+    private ListPreference prefUpdateInterval;
+    private ListPreference prefAllReadBehavior;
+    private ListPreference prefSwipeDirection;
+    private SwitchPreference prefArticleSort;
+    private SwitchPreference prefInternalBrowser;
+
+    private PreferenceHelper helper;
+    private SharedPreferences.OnSharedPreferenceChangeListener listener;
 
     public SettingFragment() {
         // Required empty public constructor
@@ -15,6 +29,161 @@ public class SettingFragment extends PreferenceFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.setting_fragment);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        helper = PreferenceHelper.getInstance(getActivity());
+        initView();
+        initListener();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(listener);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(listener);
+    }
+
+    private void initView() {
+        // Set defalut value of update interval
+        prefUpdateInterval = (ListPreference)findPreference(getString(R.string.key_update_interval));
+        // Calc interval hour from saved interval second
+        int autoUpdateIntervalSecond = helper.getAutoUpdateIntervalSecond();
+        int autoUpdateIntervalHour = autoUpdateIntervalSecond / (60 * 60);
+        String updateIntervalHourItems[] = getResources().getStringArray(R.array.update_interval_items_values);
+        // Set index of saved interval
+        for (int i = 0; i < updateIntervalHourItems.length; i++) {
+            if (Integer.valueOf(updateIntervalHourItems[i]) == autoUpdateIntervalHour) {
+                prefUpdateInterval.setValueIndex(i);
+                // Set summary of saved interval
+                String updateIntervalStringItems[] = getResources().getStringArray(R.array.update_interval_items);
+                prefUpdateInterval.setSummary(updateIntervalStringItems[i]);
+                break;
+            }
+        }
+
+        // Set default value of article sort option
+        prefArticleSort = (SwitchPreference)findPreference(getString(R.string.key_article_sort));
+        prefArticleSort.setChecked(helper.getSortNewArticleTop());
+
+        // Set default value of internal browser option
+        prefInternalBrowser = (SwitchPreference)findPreference(getString(R.string.key_internal_browser));
+        prefInternalBrowser.setChecked(helper.isOpenInternal());
+
+        prefAllReadBehavior = (ListPreference)findPreference(getString(R.string.key_all_read_behavior));
+        String allReadBehaviorItems[] = getResources().getStringArray(R.array.all_read_behavior_values);
+        // Set index of behavior of all read
+        for (int i = 0; i < allReadBehaviorItems.length; i++) {
+            boolean allBehaviorItemBool = (Integer.valueOf(allReadBehaviorItems[i]) == 1);
+            boolean savedValue = helper.getAllReadBack();
+            if (allBehaviorItemBool == savedValue) {
+                prefAllReadBehavior.setValueIndex(i);
+                // Set summary of behavior of all read
+                String allReadBehaviorStringItems[] = getResources().getStringArray(R.array.all_read_behavior);
+                prefAllReadBehavior.setSummary(allReadBehaviorStringItems[i]);
+                break;
+            }
+        }
+
+        prefSwipeDirection = (ListPreference)findPreference(getString(R.string.key_swipe_direction));
+        String swipeDirectionItems[] = getResources().getStringArray(R.array.swipe_direction_items_values);
+        // Set index of swipe direction
+        for (int i = 0; i < swipeDirectionItems.length; i++) {
+            if (Integer.valueOf(swipeDirectionItems[i]) == helper.getSwipeDirection()) {
+                prefSwipeDirection.setValueIndex(i);
+                // Set summary of swipe direction
+                String swipeDirectionStringItems[] = getResources().getStringArray(R.array.swipe_direction_items);
+                prefSwipeDirection.setSummary(swipeDirectionStringItems[i]);
+                break;
+            }
+        }
+    }
+
+    private void initListener() {
+        listener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+            @Override
+            public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+                if (key.equals(getString(R.string.key_update_interval))) {
+                    updateUpdateInterval();
+                } else if (key.equals(getString(R.string.key_all_read_behavior))) {
+                    updateAllReadBehavior();
+                } else if (key.equals(getString(R.string.key_swipe_direction))) {
+                    updateSwipeDirection();
+                } else if (key.equals(getString(R.string.key_article_sort))) {
+                    updateArticleSort();
+                } else if (key.equals(getString(R.string.key_internal_browser))) {
+                    updateInternalBrowser();
+                }
+            }
+        };
+    }
+
+    public void updateUpdateInterval() {
+        // Save new interval second
+        int intervalHour = Integer.valueOf(prefUpdateInterval.getValue());
+        int intervalSecond = intervalHour * 60 * 60;
+        helper.setAutoUpdateIntervalSecond(intervalSecond);
+        String updateIntervalHourItems[] = getResources().getStringArray(R.array.update_interval_items_values);
+
+        // Refresh summary
+        for (int i = 0; i < updateIntervalHourItems.length; i++) {
+            if (Integer.valueOf(updateIntervalHourItems[i]) == intervalHour) {
+                String updateIntervalStringItems[] = getResources().getStringArray(R.array.update_interval_items);
+                prefUpdateInterval.setSummary(updateIntervalStringItems[i]);
+                break;
+            }
+        }
+
+        // Set new alarm
+        AlarmManagerTaskManager.setNewAlarm(getActivity());
+    }
+
+    public void updateAllReadBehavior() {
+        // Save new behavior of all read
+        boolean isAllReadBack = (Integer.valueOf(prefAllReadBehavior.getValue()) == 1);
+        helper.setAllReadBack(isAllReadBack);
+        String allReadBehaviorItems[] = getResources().getStringArray(R.array.all_read_behavior_values);
+
+        // Refresh summary
+        for (int i = 0; i < allReadBehaviorItems.length; i++) {
+            boolean allBehaviorItemBool = (Integer.valueOf(allReadBehaviorItems[i]) == 1);
+            if (allBehaviorItemBool == isAllReadBack) {
+                String allReadBehaviorStringItems[] = getResources().getStringArray(R.array.all_read_behavior);
+                prefAllReadBehavior.setSummary(allReadBehaviorStringItems[i]);
+                break;
+            }
+        }
+    }
+
+    public void updateSwipeDirection() {
+        // Save new swipe direction
+        int swipeDirection = Integer.valueOf(prefSwipeDirection.getValue());
+        helper.setSwipeDirection(swipeDirection);
+        String swipeDirectionItems[] = getResources().getStringArray(R.array.swipe_direction_items_values);
+
+        // Refresh summary
+        for (int i = 0; i < swipeDirectionItems.length; i++) {
+            if (Integer.valueOf(swipeDirectionItems[i]) == swipeDirection) {
+                String swipeDirectionStringItems[] = getResources().getStringArray(R.array.swipe_direction_items);
+                prefSwipeDirection.setSummary(swipeDirectionStringItems[i]);
+                break;
+            }
+        }
+    }
+
+    public void updateArticleSort() {
+        helper.setSortNewArticleTop(prefArticleSort.isChecked());
+    }
+
+    public void updateInternalBrowser() {
+        helper.setOpenInternal(prefInternalBrowser.isChecked());
     }
 
 }
