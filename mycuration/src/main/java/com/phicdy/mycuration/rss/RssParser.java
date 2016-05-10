6a2f5d99eefcd84022c84495a9787cd5d821b9f8
phicdy <phicdy@gmail.com>
2016-05-10 22:09:44 +0900

    Set user agent to get PC site

--- mycuration/src/main/java/com/phicdy/mycuration/rss/RssParser.java
+++ mycuration/src/main/java/com/phicdy/mycuration/rss/RssParser.java
@@ -52,7 +52,8 @@ public class RssParser {
 						sendFailAddFeedUrlBroadcast(NetworkTaskManager.ERROR_INVALID_URL);
 						return;
 					}
-					Document document = Jsoup.connect(baseUrl).get();
+					String pcUserAgent = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.63 Safari/537.36";
+					Document document = Jsoup.connect(baseUrl).userAgent(pcUserAgent).get();
 					if (!document.getElementsByTag("rdf").isEmpty() || !document.getElementsByTag("rdf:rdf").isEmpty()) {
 						Log.d(LOG_TAG, "RSS 1.0");
 						// RSS 1.0
