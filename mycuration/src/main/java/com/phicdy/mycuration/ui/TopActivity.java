43ed9281ea1e236a881767a1f7e0dbb6a191b922
phicdy <phicdy@gmail.com>
2016-10-12 23:35:52 +0900

    Replace tab with TabLayout

--- mycuration/src/main/java/com/phicdy/mycuration/ui/TopActivity.java
+++ mycuration/src/main/java/com/phicdy/mycuration/ui/TopActivity.java
@@ -3,9 +3,9 @@ package com.phicdy.mycuration.ui;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Point;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -14,18 +14,10 @@ import android.support.v4.view.MenuItemCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
-import android.view.Display;
 import android.view.KeyEvent;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.WindowManager;
-import android.widget.FrameLayout;
-import android.widget.HorizontalScrollView;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 
 import com.phicdy.mycuration.R;
 import com.phicdy.mycuration.alarm.AlarmManagerTaskManager;
@@ -38,32 +30,13 @@ import uk.co.deanwild.materialshowcaseview.MaterialShowcaseView;
 
 public class TopActivity extends AppCompatActivity implements FeedListFragment.OnFeedListFragmentListener, CurationListFragment.OnCurationListFragmentListener{
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    SectionsPagerAdapter mSectionsPagerAdapter;
-
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    ViewPager mViewPager;
+    private ViewPager mViewPager;
 
     private DatabaseAdapter dbAdapter;
     private Intent intent;
 
     private CurationListFragment curationFragment;
     private SearchView searchView;
-    private ViewGroup track;
-    private HorizontalScrollView trackScroller;
-    private View indicator;
-    private static final int INDICATOR_OFFSET_DP = 48;
-    private int indicatorOffset;
-    private int selectedPosition = POSITION_FEED_FRAGMENT;
 
     private GATrackerHelper gaTrackerHelper;
 
@@ -82,51 +55,25 @@ public class TopActivity extends AppCompatActivity implements FeedListFragment.O
         setContentView(R.layout.activity_top);
 
         curationFragment = new CurationListFragment();
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.pager);
-        if (mViewPager != null) {
-            mViewPager.setAdapter(mSectionsPagerAdapter);
-        }
-        mViewPager.addOnPageChangeListener(new PageChangeListener());
-        track = (ViewGroup)findViewById(R.id.track);
-        trackScroller = (HorizontalScrollView)findViewById(R.id.track_scroller);
-        indicator = findViewById(R.id.indicator);
-
-        WindowManager wm = getWindowManager();
-        Display disp = wm.getDefaultDisplay();
-        Point size = new Point();
-        disp.getSize(size);
-        int displayWidth = size.x;
-        LayoutInflater inflater = LayoutInflater.from(this);
-        for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
-            final int position = i;
-            ImageView ivTab = (ImageView)inflater.inflate(R.layout.tab_item, track, false);
-            ivTab.setImageResource(mSectionsPagerAdapter.getImageResource(position));
-            ivTab.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    mViewPager.setCurrentItem(position);
-                }
-            });
-            final LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams)ivTab.getLayoutParams();
-            layoutParams.width = displayWidth / mSectionsPagerAdapter.getCount();
-            ivTab.setLayoutParams(layoutParams);
-            track.addView(ivTab);
-        }
-
-        final float density = getResources().getDisplayMetrics().density;
-        indicatorOffset = (int)(INDICATOR_OFFSET_DP * density);
-
         setTitle(getString(R.string.home));
-
+        initViewPager();
         dbAdapter = DatabaseAdapter.getInstance(getApplicationContext());
         setAlarmManager();
-
         gaTrackerHelper = GATrackerHelper.getInstance(this);
+    }
+
+    private void initViewPager() {
+        SectionsPagerAdapter mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(mViewPager);
+
+        // Set icon
+        for (int i = 0; i < tabLayout.getTabCount(); i++) {
+            tabLayout.getTabAt(i).setIcon(mSectionsPagerAdapter.getImageResource(i));
+        }
+
     }
 
     @Override
@@ -202,7 +149,7 @@ public class TopActivity extends AppCompatActivity implements FeedListFragment.O
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.add:
-                switch (selectedPosition) {
+                switch (mViewPager.getCurrentItem()) {
                     case POSITION_CURATION_FRAGMENT:
                         if (dbAdapter.getNumOfFeeds() == 0) {
                             goToFeedSearch();
@@ -290,11 +237,7 @@ public class TopActivity extends AppCompatActivity implements FeedListFragment.O
         startActivity(intent);
     }
 
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+    private class SectionsPagerAdapter extends FragmentPagerAdapter {
 
         SectionsPagerAdapter(FragmentManager fm) {
             super(fm);
@@ -342,53 +285,6 @@ public class TopActivity extends AppCompatActivity implements FeedListFragment.O
                     return R.drawable.tab_filter;
             }
             return -1;
-        }
-    }
-
-    private class PageChangeListener implements ViewPager.OnPageChangeListener {
-
-        private int scrollState = ViewPager.SCROLL_STATE_IDLE;
-
-        @Override
-        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-            updateIndicatorPosition(position, positionOffset);
-            setTitle(mSectionsPagerAdapter.getPageTitle(position));
-        }
-
-        @Override
-        public void onPageSelected(int position) {
-            if (scrollState == ViewPager.SCROLL_STATE_IDLE) {
-                updateIndicatorPosition(position, 0);
-                setTitle(mSectionsPagerAdapter.getPageTitle(position));
-            }
-            gaTrackerHelper.sendScreen(mSectionsPagerAdapter.getPageTitle(position).toString());
-        }
-
-        @Override
-        public void onPageScrollStateChanged(int state) {
-            scrollState = state;
-        }
-
-        private void updateIndicatorPosition(int position, float positionOffset) {
-            final View currentView = track.getChildAt(position);
-            final View nextView = position == (track.getChildCount() -1) ? null :
-                    track.getChildAt(position + 1);
-            int leftWidthOfCurrentView = currentView.getLeft();
-            int currentWidth = currentView.getWidth();
-            int nextWidth = nextView == null ? currentWidth : nextView.getWidth();
-
-            int indicatorWidth = (int)(nextWidth * positionOffset +
-                    currentWidth * (1 - positionOffset));
-            int indicatorLeft = (int)(leftWidthOfCurrentView + positionOffset * currentWidth);
-
-            final FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams)indicator.getLayoutParams();
-            layoutParams.width = indicatorWidth;
-            layoutParams.setMargins(indicatorLeft, 0, 0, 0);
-            indicator.setLayoutParams(layoutParams);
-
-            trackScroller.scrollTo(indicatorLeft - indicatorOffset, 0);
-
-            selectedPosition = position;
         }
     }
 }
