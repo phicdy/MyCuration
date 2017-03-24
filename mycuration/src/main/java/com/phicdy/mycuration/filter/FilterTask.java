2f743a3a6e6ec43092a6ff13ef2c95658422253f
phicdy <phicdy@gmail.com>
2017-03-25 01:34:43 +0800

    [Lint] Fix error

--- mycuration/src/main/java/com/phicdy/mycuration/filter/FilterTask.java
+++ mycuration/src/main/java/com/phicdy/mycuration/filter/FilterTask.java
@@ -6,20 +6,11 @@ import com.phicdy.mycuration.db.DatabaseAdapter;
 
 import android.content.Context;
   
-/**
- * 
- * @author phicdy
- * @param  String : data type for task(ex.download URL)
- * @param  String : data type for displaying the progress
- * @param  Boolean : submit data type when task will finish 
- */
 public class FilterTask {
-  
-    private Context context_;
+
     private DatabaseAdapter dbAdapter;
       
     public FilterTask(Context context) {
-        context_ = context;
         dbAdapter = DatabaseAdapter.getInstance(context);
     }
   
