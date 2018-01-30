02f4bb0dbcfe3ef0a16bada78a656499a477d9fb
phicdy <phicdy@gmail.com>
2018-01-30 23:23:35 +0900

    Kotlin: Convert MyApplication to Kotlin

--- mycuration/src/main/java/com/phicdy/mycuration/MyApplication.kt
+++ mycuration/src/main/java/com/phicdy/mycuration/MyApplication.kt
@@ -1,48 +1,45 @@
-package com.phicdy.mycuration;
+package com.phicdy.mycuration
 
-import android.app.Application;
+import android.app.Application
+import android.content.Context
 
-import com.facebook.stetho.Stetho;
-import com.google.android.gms.analytics.GoogleAnalytics;
-import com.google.android.gms.analytics.Tracker;
-import com.phicdy.mycuration.tracker.GATrackerHelper;
+import com.facebook.stetho.Stetho
+import com.google.android.gms.analytics.GoogleAnalytics
+import com.google.android.gms.analytics.Tracker
+import com.phicdy.mycuration.tracker.GATrackerHelper
 
-import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig
 
-public class MyApplication extends Application{
-    private Tracker mTracker;
+class MyApplication : Application() {
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        GATrackerHelper.setTracker(getDefaultTracker());
-        GATrackerHelper.setCategoryAction(getString(R.string.action));
-        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
-                        .setDefaultFontPath("fonts/GenShinGothic-P-Regular.ttf")
-                        .setFontAttrId(R.attr.fontPath)
-                        .build()
-        );
+    /**
+     * Gets the default [Tracker] for this [Application].
+     * @return tracker
+     */
+    // To enable debug logging use: adb shell setprop log.tag.GAv4 DEBUG
+    companion object DefaultTracker {
+        fun setUp(context: Context): Tracker {
+            val analytics = GoogleAnalytics.getInstance(context)
+            return analytics.newTracker(R.xml.global_tracker)
+        }
+    }
+    override fun onCreate() {
+        super.onCreate()
+        GATrackerHelper.setTracker(DefaultTracker.setUp(this))
+        GATrackerHelper.setCategoryAction(getString(R.string.action))
+        CalligraphyConfig.initDefault(CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/GenShinGothic-P-Regular.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build()
+        )
         if (BuildConfig.DEBUG) {
             Stetho.initialize(
                     Stetho.newInitializerBuilder(this)
-                        .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
-                        .enableWebKitInspector(Stetho.defaultInspectorModulesProvider(this))
-                        .build()
-            );
+                            .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+                            .enableWebKitInspector(Stetho.defaultInspectorModulesProvider(this))
+                            .build()
+            )
         }
-    }
-
-    /**
-     * Gets the default {@link Tracker} for this {@link Application}.
-     * @return tracker
-     */
-    synchronized public Tracker getDefaultTracker() {
-        if (mTracker == null) {
-            GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
-            // To enable debug logging use: adb shell setprop log.tag.GAv4 DEBUG
-            mTracker = analytics.newTracker(R.xml.global_tracker);
-        }
-        return mTracker;
     }
 
 }
