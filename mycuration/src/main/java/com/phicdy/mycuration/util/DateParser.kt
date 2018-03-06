554e94a1fe84920eb6947e383c9f4586a678cdd2
phicdy <phicdy@gmail.com>
2018-03-06 21:46:00 +0900

    Date Parser: Fix indent and not print unused log

--- mycuration/src/main/java/com/phicdy/mycuration/util/DateParser.kt
+++ mycuration/src/main/java/com/phicdy/mycuration/util/DateParser.kt
@@ -10,7 +10,7 @@ import android.util.Log
 
 object DateParser {
 
-	private val LOG_TAG = "DateParser"
+	private const val LOG_TAG = "DateParser"
 
 	private fun parseDate(pubDate: String): Date? {
 		val input = SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss Z",
@@ -18,53 +18,43 @@ object DateParser {
 		var formatWithPubDate: Date? = null
 		try {
 			formatWithPubDate = input.parse(pubDate)
+            return formatWithPubDate
 		} catch (e: ParseException) {
-			e.printStackTrace()
 		}
 
-		if (formatWithPubDate == null) {
-			//2014-06-25 17:24:07
-			// TODO: set device locale
-			val noTimezone = SimpleDateFormat("yyyy-MM-dd HH:mm:ss",
-					Locale.JAPAN)
-			var formatWithNoTimeZone: Date? = null
-			try {
-				formatWithNoTimeZone = noTimezone.parse(pubDate)
-			} catch (e2: ParseException) {
-				e2.printStackTrace()
-			}
+        //2014-06-25 17:24:07
+        // TODO: set device locale
+        val noTimezone = SimpleDateFormat("yyyy-MM-dd HH:mm:ss",
+                Locale.JAPAN)
+        var formatWithNoTimeZone: Date? = null
+        try {
+            formatWithNoTimeZone = noTimezone.parse(pubDate)
+            return formatWithNoTimeZone
+        } catch (e2: ParseException) {
+        }
 
-			if (formatWithNoTimeZone == null) {
-				//2014-07-27T14:38:34+09:00
-				if (pubDate.contains("T")) {
-					var replaced = pubDate.replace("T", " ")
-					if (pubDate.contains("Z")) {
-						//2014-07-27T14:38:34Z
-						replaced = replaced.replace("Z", "+09:00")
-					}
+        //2014-07-27T14:38:34+09:00
+        if (!pubDate.contains("T")) {
+            Log.d(LOG_TAG, "Invalid format, return null")
+            return null
+        }
+        var replaced = pubDate.replace("T", " ")
+        if (pubDate.contains("Z")) {
+            //2014-07-27T14:38:34Z
+            replaced = replaced.replace("Z", "+09:00")
+        }
 
-					val w3cdtf = SimpleDateFormat("yyyy-MM-dd HH:mm:ssZ",
-							Locale.US)
-					var formatWithW3cdtf: Date? = null
-					try {
-						formatWithW3cdtf = w3cdtf.parse(replaced)
-					} catch (e: ParseException) {
-						e.printStackTrace()
-					}
+        val w3cdtf = SimpleDateFormat("yyyy-MM-dd HH:mm:ssZ",
+                Locale.US)
+        var formatWithW3cdtf: Date? = null
+        try {
+            formatWithW3cdtf = w3cdtf.parse(replaced)
+            return formatWithW3cdtf
+        } catch (e: ParseException) {
+        }
 
-					return if (formatWithW3cdtf == null) {
-						null
-					} else {
-						formatWithW3cdtf
-					}
-				}
-			} else {
-				return formatWithNoTimeZone
-			}
-		} else {
-			return formatWithPubDate
-		}
-		return null
+        Log.d(LOG_TAG, "Contains T, but failed to parse")
+        return null
 	}
 
 	fun changeToJapaneseDate(dateBeforeChange: String): Long {
