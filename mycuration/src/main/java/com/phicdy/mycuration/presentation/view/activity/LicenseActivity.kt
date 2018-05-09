5959552a7d625236e02b133df7cc0e6afc27944e
phicdy <phicdy@gmail.com>
2018-05-09 21:02:34 +0900

    Kotlin: Convert LicenseActivity to Kotlin

---
+++ mycuration/src/main/java/com/phicdy/mycuration/presentation/view/activity/LicenseActivity.kt
@@ -0,0 +1,27 @@
+package com.phicdy.mycuration.presentation.view.activity
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.webkit.WebView
+
+import com.phicdy.mycuration.R
+
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper
+
+class LicenseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_license)
+
+        title = getString(R.string.license)
+        val webView = findViewById(R.id.license_web_view) as WebView
+        webView.loadUrl("file:///android_asset/license.html")
+    }
+
+    override fun attachBaseContext(newBase: Context) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase))
+    }
+
+}
