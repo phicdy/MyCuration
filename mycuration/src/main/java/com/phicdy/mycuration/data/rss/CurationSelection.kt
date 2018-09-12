840add0d77400aaac7d5be2ec1fc4b0dcef41132
phicdy <phicdy@gmail.com>
2018-09-12 21:19:30 +0900

    Kotlin: Convert CurationSelection to Kotlin

---
+++ mycuration/src/main/java/com/phicdy/mycuration/data/rss/CurationSelection.kt
@@ -0,0 +1,15 @@
+package com.phicdy.mycuration.data.rss
+
+object CurationSelection {
+    const val TABLE_NAME = "curationSelections"
+    const val CURATION_ID = "curationId"
+    const val ARTICLE_ID = "articleId"
+    private const val ID = "_id"
+
+    const val CREATE_TABLE_SQL = "create table " + TABLE_NAME + "(" +
+            ID + " integer primary key autoincrement," +
+            CURATION_ID + " integer," +
+            ARTICLE_ID + " integer," +
+            "foreign key(" + CURATION_ID + ") references " + Curation.TABLE_NAME + "(" + Curation.ID + ")," +
+            "foreign key(" + ARTICLE_ID + ") references " + Article.TABLE_NAME + "(" + Article.ID + "))"
+}
