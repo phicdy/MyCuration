a9b6cfecd071d5017793b6a70c1fb0c6635d21b5
phicdy <phicdy@gmail.com>
2018-10-24 21:31:01 +0900

    Auto update: Replace getAllFeedsWith/WithoutNumOfUnreadArticles with coroutine

--- mycuration/src/main/java/com/phicdy/mycuration/domain/alarm/AutoUpdateBroadcastReciever.kt
+++ mycuration/src/main/java/com/phicdy/mycuration/domain/alarm/AutoUpdateBroadcastReciever.kt
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import com.phicdy.mycuration.data.db.DatabaseAdapter
+import com.phicdy.mycuration.data.repository.RssRepository
 import com.phicdy.mycuration.data.rss.Article
 import com.phicdy.mycuration.data.rss.Feed
 import com.phicdy.mycuration.domain.task.GetHatenaBookmark
@@ -11,59 +12,76 @@ import com.phicdy.mycuration.domain.task.NetworkTaskManager
 import com.phicdy.mycuration.util.NetworkUtil
 import com.phicdy.mycuration.util.PreferenceHelper
 import io.reactivex.schedulers.Schedulers
+import kotlinx.coroutines.experimental.GlobalScope
+import kotlinx.coroutines.experimental.coroutineScope
+import kotlinx.coroutines.experimental.launch
+import org.koin.standalone.KoinComponent
+import org.koin.standalone.inject
 import org.reactivestreams.Subscriber
 import org.reactivestreams.Subscription
 
-class AutoUpdateBroadcastReciever : BroadcastReceiver() {
+class AutoUpdateBroadcastReciever : BroadcastReceiver(), KoinComponent {
+
+    private val rssRepository: RssRepository by inject()
 
     override fun onReceive(context: Context, intent: Intent?) {
-        if (intent == null || intent.action == null) return
-        val dbAdapter = DatabaseAdapter.getInstance()
-        if (intent.action == AUTO_UPDATE_ACTION) {
-            val updateTask = NetworkTaskManager
-            val feeds = dbAdapter.allFeedsWithoutNumOfUnreadArticles
-            updateTask.updateAllFeeds(feeds)
-                    .observeOn(Schedulers.io())
-                    .subscribe(object : Subscriber<Feed> {
-                        override fun onSubscribe(s: Subscription) {
-                            s.request(feeds.size.toLong())
-                        }
-
-                        override fun onNext(feed: Feed) {}
-
-                        override fun onError(t: Throwable) {}
-
-                        override fun onComplete() {}
-                    })
-            val manager = AlarmManagerTaskManager(context)
-            manager.setNewHatenaUpdateAlarmAfterFeedUpdate(context)
-
-            // Save new time
-            manager.setNewAlarm(PreferenceHelper.autoUpdateIntervalSecond)
-        } else if (intent.action == AUTO_UPDATE_HATENA_ACTION) {
-            // Update Hatena point
-            val feeds = dbAdapter.allFeedsWithNumOfUnreadArticles
-            if (feeds.isEmpty()) return
-
-            // Update has higher priority
-            if (NetworkTaskManager.isUpdatingFeed) {
-                AlarmManagerTaskManager(context).setNewHatenaUpdateAlarmAfterFeedUpdate(context)
-                return
+        GlobalScope.launch {
+            if (intent == null || intent.action == null) return@launch
+            if (intent.action == AUTO_UPDATE_ACTION) {
+                handleAutoUpdate(context)
+            } else if (intent.action == AUTO_UPDATE_HATENA_ACTION) {
+                handleUpdateHatena(context)
             }
+        }
+    }
 
-            val isWifiConnected = NetworkUtil.isWifiConnected(context)
-            val getHatenaBookmark = GetHatenaBookmark(dbAdapter)
-            var delaySec = 0
-            var totalNum = 0
-            for (feed in feeds) {
-                val unreadArticles = dbAdapter.getUnreadArticlesInAFeed(feed.id, true)
-                if (unreadArticles.isEmpty()) continue
-                for (unreadArticle in unreadArticles) {
-                    if (unreadArticle.point != Article.DEDAULT_HATENA_POINT && !isWifiConnected) continue
-                    totalNum++
-                    getHatenaBookmark.request(unreadArticle.url, delaySec)
-                    if (totalNum % 10 == 0) delaySec += 2
-                }
+    private suspend fun handleAutoUpdate(context: Context) = coroutineScope {
+        val updateTask = NetworkTaskManager
+        val feeds = rssRepository.getAllFeedsWithoutNumOfUnreadArticles()
+        updateTask.updateAllFeeds(feeds)
+                .observeOn(Schedulers.io())
+                .subscribe(object : Subscriber<Feed> {
+                    override fun onSubscribe(s: Subscription) {
+                        s.request(feeds.size.toLong())
+                    }
+
+                    override fun onNext(feed: Feed) {}
+
+                    override fun onError(t: Throwable) {}
+
+                    override fun onComplete() {}
+                })
+        val manager = AlarmManagerTaskManager(context)
+        manager.setNewHatenaUpdateAlarmAfterFeedUpdate(context)
+
+        // Save new time
+        manager.setNewAlarm(PreferenceHelper.autoUpdateIntervalSecond)
+    }
+
+    private suspend fun handleUpdateHatena(context: Context) {
+        val dbAdapter = DatabaseAdapter.getInstance()
+        // Update Hatena point
+        val feeds = rssRepository.getAllFeedsWithNumOfUnreadArticles()
+        if (feeds.isEmpty()) return
+
+        // Update has higher priority
+        if (NetworkTaskManager.isUpdatingFeed) {
+            AlarmManagerTaskManager(context).setNewHatenaUpdateAlarmAfterFeedUpdate(context)
+            return
+        }
+
+        val isWifiConnected = NetworkUtil.isWifiConnected(context)
+        val getHatenaBookmark = GetHatenaBookmark(dbAdapter)
+        var delaySec = 0
+        var totalNum = 0
+        for (feed in feeds) {
+            val unreadArticles = dbAdapter.getUnreadArticlesInAFeed(feed.id, true)
+            if (unreadArticles.isEmpty()) continue
+            for (unreadArticle in unreadArticles) {
+                if (unreadArticle.point != Article.DEDAULT_HATENA_POINT && !isWifiConnected) continue
+                totalNum++
+                getHatenaBookmark.request(unreadArticle.url, delaySec)
+                if (totalNum % 10 == 0) delaySec += 2
             }
         }
     }
