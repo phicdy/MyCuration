b67ec3ec044d0dfeff27c63212bf6fbb761cae04
phicdy <phicdy@gmail.com>
2018-11-12 00:53:04 +0900

    Refactor: Use support library

--- mycuration/src/main/java/com/phicdy/mycuration/presentation/view/fragment/MyProgressDialogFragment.kt
+++ mycuration/src/main/java/com/phicdy/mycuration/presentation/view/fragment/MyProgressDialogFragment.kt
@@ -1,9 +1,9 @@
 package com.phicdy.mycuration.presentation.view.fragment
 
 import android.app.Dialog
-import android.app.DialogFragment
 import android.app.ProgressDialog
 import android.os.Bundle
+import android.support.v4.app.DialogFragment
 
 class MyProgressDialogFragment : DialogFragment() {
 
