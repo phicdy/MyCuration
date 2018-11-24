10f18499f816e2bb0c9cb418f32d4397973a7d0f
phicdy <phicdy@gmail.com>
2018-11-24 19:14:19 +0900

    Setting: Show arrow button to go back

--- mycuration/src/main/java/com/phicdy/mycuration/presentation/view/activity/SettingActivity.kt
+++ mycuration/src/main/java/com/phicdy/mycuration/presentation/view/activity/SettingActivity.kt
@@ -2,25 +2,36 @@ package com.phicdy.mycuration.presentation.view.activity
 
 import android.content.Context
 import android.os.Bundle
-import android.preference.PreferenceActivity
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
+import com.phicdy.mycuration.R
 
-import com.phicdy.mycuration.presentation.view.fragment.SettingFragment
 
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper
 
 class SettingActivity : AppCompatActivity() {
 
-    private val fragment = SettingFragment()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        supportFragmentManager.beginTransaction()
-                .replace(android.R.id.content, fragment)
-                .commit()
+        setContentView(R.layout.activity_setting)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar_setting)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = getString(R.string.setting)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
     }
 
     override fun attachBaseContext(newBase: Context) {
         super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase))
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            // For arrow button on toolbar
+            android.R.id.home -> finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
