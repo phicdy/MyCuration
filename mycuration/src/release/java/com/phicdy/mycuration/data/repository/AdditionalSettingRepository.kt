8869399775e60a536bb40b810d46d19b50a3a375
phicdy <phicdy@gmail.com>
2019-06-20 21:55:36 +0900

    Fix: Add missing AlarmTaskManager

--- mycuration/src/release/java/com/phicdy/mycuration/data/repository/AdditionalSettingRepository.kt
+++ mycuration/src/release/java/com/phicdy/mycuration/data/repository/AdditionalSettingRepository.kt
@@ -1,8 +1,12 @@
 package com.phicdy.mycuration.data.repository
 
+import com.phicdy.mycuration.domain.alarm.AlarmManagerTaskManager
 import java.io.File
 
-class AdditionalSettingRepository(private val rssRepository: RssRepository) : AdditionalSettingApi {
+class AdditionalSettingRepository(
+        private val rssRepository: RssRepository,
+        private val alarmManagerTaskManager: AlarmManagerTaskManager
+) : AdditionalSettingApi {
     override suspend fun exportDb(currentDb: File) {
     }
 
