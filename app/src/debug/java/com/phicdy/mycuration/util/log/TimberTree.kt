f36637d8671190a5c4bae503495957be35a33af9
phicdy <phicdy@gmail.com>
2019-06-23 16:37:18 +0900

    Refactor: Auto format

--- app/src/debug/java/com/phicdy/mycuration/util/log/TimberTree.kt
+++ app/src/debug/java/com/phicdy/mycuration/util/log/TimberTree.kt
@@ -5,7 +5,6 @@ import timber.log.Timber
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
-import java.lang.StringBuilder
 import java.util.Date
 
 
--- legacy/src/main/java/com/phicdy/mycuration/util/FileUtil.kt
+++ legacy/src/main/java/com/phicdy/mycuration/util/FileUtil.kt
@@ -10,80 +10,80 @@ import java.util.Scanner
 
 object FileUtil {
 
-	// Get mount information
-	val sdCardRootPath: String?
-		get() {
-			var sdCardRootPath: String? = null
-			var scanner: Scanner? = null
-			try {
-				val fstab = File("/system/etc/vold.fstab")
-				if (fstab.exists()) {
-					scanner = Scanner(FileInputStream(File(
-							"/system/etc/vold.fstab")))
-					while (scanner.hasNextLine()) {
-						val line = scanner.nextLine()
-						if (line.startsWith("dev_mount") || line.startsWith("fuse_mount")) {
-							sdCardRootPath = line.replace("\t".toRegex(), " ").split(" ".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[2]
-						}
-					}
-				} else {
-					sdCardRootPath = Environment.getExternalStorageDirectory().path
-				}
-			} catch (e: FileNotFoundException) {
-				throw RuntimeException(e)
-			} finally {
-				scanner?.close()
-			}
+    // Get mount information
+    val sdCardRootPath: String?
+        get() {
+            var sdCardRootPath: String? = null
+            var scanner: Scanner? = null
+            try {
+                val fstab = File("/system/etc/vold.fstab")
+                if (fstab.exists()) {
+                    scanner = Scanner(FileInputStream(File(
+                            "/system/etc/vold.fstab")))
+                    while (scanner.hasNextLine()) {
+                        val line = scanner.nextLine()
+                        if (line.startsWith("dev_mount") || line.startsWith("fuse_mount")) {
+                            sdCardRootPath = line.replace("\t".toRegex(), " ").split(" ".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[2]
+                        }
+                    }
+                } else {
+                    sdCardRootPath = Environment.getExternalStorageDirectory().path
+                }
+            } catch (e: FileNotFoundException) {
+                throw RuntimeException(e)
+            } finally {
+                scanner?.close()
+            }
 
-			return sdCardRootPath
-		}
+            return sdCardRootPath
+        }
 
-	fun isSDCardMouted(path: String): Boolean {
-		var isMounted = false
-		var scanner: Scanner? = null
-		try {
-			// Get mount poing
-			val file = File("/proc/mounts")
-			scanner = Scanner(FileInputStream(file))
+    fun isSDCardMouted(path: String): Boolean {
+        var isMounted = false
+        var scanner: Scanner? = null
+        try {
+            // Get mount poing
+            val file = File("/proc/mounts")
+            scanner = Scanner(FileInputStream(file))
 
-			// Check path in mounted points
-			while (scanner.hasNextLine()) {
-				if (scanner.nextLine().contains(path)) {
-					isMounted = true
-					break
-				}
-			}
-		} catch (e: FileNotFoundException) {
-			throw RuntimeException(e)
-		} finally {
-			scanner?.close()
-		}
-		return isMounted
-	}
+            // Check path in mounted points
+            while (scanner.hasNextLine()) {
+                if (scanner.nextLine().contains(path)) {
+                    isMounted = true
+                    break
+                }
+            }
+        } catch (e: FileNotFoundException) {
+            throw RuntimeException(e)
+        } finally {
+            scanner?.close()
+        }
+        return isMounted
+    }
 
-	fun getAppPath(context: Context): String {
-		val pkgMgr = context.packageManager
-		try {
-			val path = pkgMgr.getPackageInfo(context.packageName, 0).applicationInfo.dataDir
-			return if (path.endsWith("/")) {
-				path
-			} else {
-				"$path/"
-			}
-		} catch (e: NameNotFoundException) {
-			e.printStackTrace()
-		}
+    fun getAppPath(context: Context): String {
+        val pkgMgr = context.packageManager
+        try {
+            val path = pkgMgr.getPackageInfo(context.packageName, 0).applicationInfo.dataDir
+            return if (path.endsWith("/")) {
+                path
+            } else {
+                "$path/"
+            }
+        } catch (e: NameNotFoundException) {
+            e.printStackTrace()
+        }
 
-		return ""
-	}
+        return ""
+    }
 
-	lateinit var appPath: String
-	fun setUpAppPath(context: Context) {
-		appPath = getAppPath(context)
-	}
+    lateinit var appPath: String
+    fun setUpAppPath(context: Context) {
+        appPath = getAppPath(context)
+    }
 
-	fun iconSaveFolder(): String {
-		return appPath + "icons/"
-	}
+    fun iconSaveFolder(): String {
+        return appPath + "icons/"
+    }
 
 }
