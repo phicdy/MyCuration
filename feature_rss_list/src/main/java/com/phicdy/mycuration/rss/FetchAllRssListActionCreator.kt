54b0d9371c0df7a68a8f931ce1dd4ec701e2750b
phicdy <phicdy@gmail.com>
2019-11-18 21:10:32 +0900

    RSS List: Add fetch action creator

---
+++ feature_rss_list/src/main/java/com/phicdy/mycuration/rss/FetchAllRssListActionCreator.kt
@@ -0,0 +1,36 @@
+package com.phicdy.mycuration.rss
+
+import com.phicdy.mycuration.core.ActionCreator1
+import com.phicdy.mycuration.core.Dispatcher
+import com.phicdy.mycuration.data.repository.RssRepository
+import com.phicdy.mycuration.entity.Feed
+import com.phicdy.mycuration.entity.RssListMode
+
+class FetchAllRssListActionCreator(
+        private val dispatcher: Dispatcher,
+        private val rssRepository: RssRepository
+) : ActionCreator1<RssListMode> {
+
+    override suspend fun run(arg: RssListMode) {
+        val rss = rssRepository.getAllFeedsWithNumOfUnreadArticles()
+        if (rss.isEmpty()) {
+            dispatcher.dispatch(RssListAction(RssListState(emptyList(), arg)))
+            return
+        }
+        val list = mutableListOf<RssListItem>().apply {
+            add(RssListItem.All(rss.sumBy { it.unreadAriticlesCount }))
+            add(RssListItem.Favroite)
+            rss.map {
+                this.add(RssListItem.Content(
+                        rssId = it.id,
+                        rssTitle = it.title,
+                        isDefaultIcon = it.iconPath.isBlank() || it.iconPath == Feed.DEDAULT_ICON_PATH,
+                        rssIconPath = it.iconPath,
+                        unreadCount = it.unreadAriticlesCount
+                ))
+            }
+            add(RssListItem.Footer(if (arg == RssListMode.UNREAD_ONLY) RssListFooterState.UNREAD_ONLY else RssListFooterState.ALL))
+        }
+        dispatcher.dispatch(RssListAction(RssListState(list, arg)))
+    }
+}
