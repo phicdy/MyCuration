c2aed47bf6a341564f70a3a00d42b32332636a9b
phicdy <phicdy@gmail.com>
2019-12-17 21:03:33 +0900

    RSS List: Update RSS list asynchronously

--- feature_rss_list/src/main/java/com/phicdy/mycuration/rss/UpdateAllRssActionCreator.kt
+++ feature_rss_list/src/main/java/com/phicdy/mycuration/rss/UpdateAllRssActionCreator.kt
@@ -6,7 +6,8 @@ import com.phicdy.mycuration.data.preference.PreferenceHelper
 import com.phicdy.mycuration.domain.task.NetworkTaskManager
 import com.phicdy.mycuration.entity.Feed
 import com.phicdy.mycuration.entity.RssListMode
-import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 
 class UpdateAllRssActionCreator(
         private val dispatcher: Dispatcher,
@@ -18,17 +19,21 @@ class UpdateAllRssActionCreator(
     override suspend fun run(arg1: List<Feed>, arg2: RssListMode) {
         try {
             dispatcher.dispatch(RssListUpdateAction(RssListUpdateState.Started))
-            networkTaskManager.updateAll(arg1).collect { rss ->
-                val replaced = arg1.map {
-                    if (it.id == rss.id) {
-                        rss
-                    } else {
-                        it
-                    }
-                }
-                rssListItemFactory.create(arg2, replaced).let {
-                    dispatcher.dispatch(RssListUpdateAction(RssListUpdateState.Updating(it)))
-                }
+            coroutineScope {
+                arg1.map { async { networkTaskManager.updateFeed(it) } }
+                        .map { rss ->
+                            //            networkTaskManager.updateAll(arg1).collect { rss ->
+                            val replaced = arg1.map {
+                                if (it.id == rss.await().id) {
+                                    rss.await()
+                                } else {
+                                    it
+                                }
+                            }
+                            rssListItemFactory.create(arg2, replaced).let {
+                                dispatcher.dispatch(RssListUpdateAction(RssListUpdateState.Updating(it)))
+                            }
+                        }
             }
             preferenceHelper.lastUpdateDate = System.currentTimeMillis()
             dispatcher.dispatch(RssListUpdateAction(RssListUpdateState.Finished))
