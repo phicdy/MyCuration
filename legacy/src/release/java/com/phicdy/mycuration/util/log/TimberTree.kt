7df4647992601d7d5460c7209dd08060557c5337
Keisuke Yamaguchi <phicdy@gmail.com>
2021-02-04 21:19:34 +0900

    Project: Fix release build

--- legacy/src/release/java/com/phicdy/mycuration/util/log/TimberTree.kt
+++ legacy/src/release/java/com/phicdy/mycuration/util/log/TimberTree.kt
@@ -1,8 +1,9 @@
 package com.phicdy.mycuration.util.log
 
 import timber.log.Timber
+import javax.inject.Inject
 
-class TimberTree : Timber.Tree() {
+class TimberTree @Inject constructor() : Timber.Tree() {
 
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
     }
