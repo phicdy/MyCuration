c3ecd0aca98b34676b42ee85fa7ba1a085f63dec
Keisuke Yamaguchi <phicdy@gmail.com>
2021-03-20 18:02:42 +0900

    Test: Fix icon test

--- app/src/androidTest/java/com/phicdy/mycuration/domain/rss/IconParserTest.kt
+++ app/src/androidTest/java/com/phicdy/mycuration/domain/rss/IconParserTest.kt
@@ -12,7 +12,7 @@ class IconParserTest {
     fun testParseXml() {
         val parser = IconParser()
         assertEquals("https://gigazine.net/favicon.ico", parser.parseHtml("https://gigazine.net"))
-        assertEquals("https://s2.wp.com/wp-content/themes/vip/techcrunch-jp-2015/assets/images/favicon.ico", parser.parseHtml("http://jp.techcrunch.com"))
+        assertEquals("https://jp.techcrunch.com/wp-content/themes/techcrunch-jp-2015/assets/images/TC_favicon.png", parser.parseHtml("http://jp.techcrunch.com"))
         assertEquals("http://b.hatena.ne.jp/favicon.ico", parser.parseHtml("http://b.hatena.ne.jp"))
     }
 }
