1790186a376ad2e82b2f558a5baf8b535a4e0ba0
Keisuke Yamaguchi <phicdy@gmail.com>
2021-06-15 22:52:31 +0900

    Database: Create curationConditions table by SQLDelight

--- repository/src/main/java/com/phicdy/mycuration/data/db/DatabaseHelper.kt
+++ repository/src/main/java/com/phicdy/mycuration/data/db/DatabaseHelper.kt
@@ -3,7 +3,6 @@ package com.phicdy.mycuration.data.db
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
-import com.phicdy.mycuration.entity.CurationCondition
 import com.phicdy.mycuration.entity.FavoriteArticle
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
@@ -18,7 +17,6 @@ class DatabaseHelper @Inject constructor(
     //onCreate() is called when database is created
     override fun onCreate(db: SQLiteDatabase) {
         db.apply {
-            execSQL(CurationCondition.CREATE_TABLE_SQL)
             execSQL(FavoriteArticle.CREATE_TABLE_SQL)
         }
     }
