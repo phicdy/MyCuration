9c3a206d5eeb219051a5cdb4e3a152477ce1f301
Keisuke Yamaguchi <phicdy@gmail.com>
2021-06-28 23:05:05 +0900

    Database: Replace get feed by title and url and format by SQLDelight

--- repository/src/main/java/com/phicdy/mycuration/data/repository/RssRepository.kt
+++ repository/src/main/java/com/phicdy/mycuration/data/repository/RssRepository.kt
@@ -1,15 +1,11 @@
 package com.phicdy.mycuration.data.repository
 
-import android.database.Cursor
-import android.database.SQLException
-import android.database.sqlite.SQLiteDatabase
 import com.phicdy.mycuration.core.CoroutineDispatcherProvider
 import com.phicdy.mycuration.data.Feeds
 import com.phicdy.mycuration.di.common.ApplicationCoroutineScope
 import com.phicdy.mycuration.entity.Feed
 import com.phicdy.mycuration.repository.Database
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.withContext
 import timber.log.Timber
 import javax.inject.Inject
@@ -17,7 +13,6 @@ import javax.inject.Singleton
 
 @Singleton
 class RssRepository @Inject constructor(
-        private val db: SQLiteDatabase,
         private val database: Database,
         private val articleRepository: ArticleRepository,
         private val filterRepository: FilterRepository,
@@ -139,13 +134,10 @@ class RssRepository @Inject constructor(
      */
     suspend fun store(feedTitle: String, feedUrl: String, format: String, siteUrl: String): Feed? = withContext(coroutineDispatcherProvider.io()) {
         withContext(applicationCoroutineScope.coroutineContext) {
-            // Get same feeds from DB
-            val selection = Feed.TITLE + "=\"$feedTitle\" and " + Feed.URL + "=\"$feedUrl\" and " +
-                    Feed.FORMAT + "=\"$format\""
-            val stored = query(arrayOf(Feed.ID), selection)
-            if (stored != null) {
-                // If there aren't same feeds in DB,Insert into DB
-                database.transactionWithResult<Feed> {
+            database.transactionWithResult {
+                val stored = database.feedQueries.getFeedByTitleAndUrlAndFormat(feedTitle, feedUrl, format).executeAsOneOrNull()
+                if (stored == null) {
+                    // If there aren't same feeds in DB,Insert into DB
                     database.feedQueries.insert(feedTitle, feedUrl, format, Feed.DEDAULT_ICON_PATH, siteUrl, 0)
                     val id = database.feedQueries.selectLastInsertRowId().executeAsOne()
                     Feed(
@@ -155,9 +147,9 @@ class RssRepository @Inject constructor(
                             format = format,
                             siteUrl = siteUrl
                     )
+                } else {
+                    null
                 }
-            } else {
-                null
             }
         }
     }
@@ -172,42 +164,6 @@ class RssRepository @Inject constructor(
         return@withContext database.transactionWithResult<Feed?> {
             database.feedQueries.getFeedById(feedId.toLong()).executeAsOneOrNull()?.toFeed()
         }
-    }
-
-    private suspend fun query(columns: Array<String>, selection: String? = null): Feed? = coroutineScope {
-        var feed: Feed? = null
-        var cur: Cursor? = null
-        try {
-            db.beginTransaction()
-            cur = db.query(Feed.TABLE_NAME, columns, selection, null, null, null, null)
-            if (cur.count != 0) {
-                cur.moveToNext()
-                var id = 0
-                var title = ""
-                var url = ""
-                var iconPath = ""
-                var siteUrl = ""
-                var count = 0
-                for ((i, column) in columns.withIndex()) {
-                    when (column) {
-                        Feed.ID -> id = cur.getInt(i)
-                        Feed.TITLE -> title = cur.getString(i)
-                        Feed.URL -> url = cur.getString(i)
-                        Feed.ICON_PATH -> iconPath = cur.getString(i)
-                        Feed.SITE_URL -> siteUrl = cur.getString(i)
-                        Feed.UNREAD_ARTICLE -> count = cur.getInt(i)
-                    }
-                }
-                feed = Feed(id, title, url, iconPath, "", count, siteUrl)
-            }
-            db.setTransactionSuccessful()
-        } catch (e: SQLException) {
-            e.printStackTrace()
-        } finally {
-            cur?.close()
-            db.endTransaction()
-        }
-        return@coroutineScope feed
     }
 
     private fun Feeds.toFeed(): Feed =
